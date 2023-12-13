9c8727cfe792d944dbb1af35d8e04be74e85c03d
Karuga Mbugua <karugambugua@icloud.com>
2023-12-13 12:44:21 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,20 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self._name = 1
+    
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance(name, str) and (1 <= len(name) <= 25):
+            print(f"{name}")
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    name = property(get_name, set_name)
+guido = Dog("go")
+print(guido.name)
+guido.name = "asdddddddddsfdsfdsfsddddddd"
